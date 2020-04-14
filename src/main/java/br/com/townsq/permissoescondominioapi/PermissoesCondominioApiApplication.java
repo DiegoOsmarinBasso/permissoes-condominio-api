a3f6e7a994203388db5b04ca230c68946f02f96b
Diego Osmarin Basso <diego_osmarim@terceiros.sicredi.com.br>
2020-04-14 19:39:42 -0300

    Projeto inicial gerado em https://start.spring.io/

---
+++ src/main/java/br/com/townsq/permissoescondominioapi/PermissoesCondominioApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.townsq.permissoescondominioapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PermissoesCondominioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PermissoesCondominioApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/townsq/permissoescondominioapi/PermissoesCondominioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.townsq.permissoescondominioapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PermissoesCondominioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
